a4f86d36eed0f8b4ff57cb35269e0970d490e8cb
phipo <sesame.philippe@gmail.com>
2021-01-19 11:28:15 +0100

    CachedRowSetImpl

--- Sql/SqlOutImpl.java
+++ Sql/SqlOutImpl.java
@@ -5,11 +5,13 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import javax.sql.rowset.CachedRowSet;
+//import javax.sql.rowset.CachedRowSet;
+import javax.sql.rowset.*;
+
 
 import org.phypo.PPg.PPgUtils.PPgPair;
 import com.sun.rowset.CachedRowSetImpl;
- 
+//import com.sun.rowset.CachedRowSetImpl;
 // **********************************
 
 public class SqlOutImpl implements SqlOut {
@@ -17,9 +19,10 @@ public class SqlOutImpl implements SqlOut {
 		public  ArrayList< PPgPair<ResultSetMetaData, CachedRowSet > > cDatas      = new  ArrayList<>();
 		
 		public  void setResult( int pNumResult,  ResultSetMetaData pMeta,  ResultSet pResultSet ) throws SQLException{
-				CachedRowSetImpl lVal = new CachedRowSetImpl();
-				lVal.populate(pResultSet);
-				cDatas.add( new PPgPair<ResultSetMetaData,CachedRowSet >(pMeta, lVal) );
+			//CachedRowSetImpl lVal = new CachedRowSetImpl();
+			CachedRowSet lVal = RowSetProvider.newFactory().createCachedRowSet();
+			lVal.populate(pResultSet);
+			cDatas.add( new PPgPair<ResultSetMetaData,CachedRowSet >(pMeta, lVal) );
 		}	
 		
 };
