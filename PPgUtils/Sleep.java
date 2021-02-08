f071b7766fb0c061a9716bebffe6011b9d9cae3c
Phipo <sesame.philippe@gmail.com>
2021-02-08 17:05:27 +0100

    add PPgUtils/Sleep.java

---
+++ PPgUtils/Sleep.java
@@ -0,0 +1,16 @@
+package org.phypo.PPg.PPgUtils;
+
+
+public class Sleep {
+	
+	// ========== just for avoid useless try catch in the code ===============
+	public static void Millisleep( long iMilli ) {
+		try {
+			Thread.sleep(iMilli);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
